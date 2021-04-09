41a7bc9dc165b0861ea8137ab7797a17eb8bc781
Pablo de la Peña <p@shedcollective.org>
2021-04-09 18:46:42 +0100

    Remove .circleci from app directory

--- src/Project/Framework/Backend/Nails.php
+++ src/Project/Framework/Backend/Nails.php
@@ -98,6 +98,7 @@ final class Nails extends Base implements Framework
             '--no-docker',
         ];
         System::exec('nails new:project ' . implode(' ', $aArguments));
+        System::exec('rm -rf ' .$sPath. 'www/.circleci');
 
         return $this;
     }
