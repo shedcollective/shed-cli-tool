b3b0c5900b5ca925f4a59e2f7ade60d4e7cb074c
Pablo de la Peña <p@shedcollective.org>
2024-08-07 15:53:56 +0100

    feat: List certificates

--- src/Entity/Heartbeat/Ssl.php
+++ src/Entity/Heartbeat/Ssl.php
@@ -2,6 +2,8 @@
 
 namespace Shed\Cli\Entity\Heartbeat;
 
+use Shed\Cli\Exceptions\HeartbeatException;
+
 /**
  * Class Ssl
  *
@@ -16,19 +18,68 @@ final class Ssl implements \JsonSerializable
      */
     public function get(): ?array
     {
-        //  @todo (Pablo 2021-08-18) - Complete this method
+        switch (Os::getType()) {
+            case Os::LINUX:
+                $certificates = $this->parseCertbotOutput(
+                    $this->getCertbotCertificates()
+                );
+                break;
+
+            case Os::MACOS:
+                $certificates = null;
+                break;
+
+            default:
+                throw new HeartbeatException('Unable to determine system load.');
+        }
         return [
-            'certificates' => [
-                [
-                    'domain'  => '',
-                    'expires' => '',
-                ],
-            ],
+            'certificates' => $certificates,
         ];
     }
 
     public function jsonSerialize(): ?array
     {
         return $this->get();
+    }
+
+    function getCertbotCertificates(): array
+    {
+        $command    = 'sudo certbot certificates 2>&1';
+        $output     = [];
+        $resultCode = 0;
+        exec($command, $output, $resultCode);
+
+        if ($resultCode !== 0) {
+            throw new HeartbeatException('Failed to retrieve certificate information.');
+        }
+
+        return $output;
+    }
+
+    function parseCertbotOutput($output): array
+    {
+        $certificates = [];
+        $currentCert  = null;
+
+        foreach ($output as $line) {
+            if (preg_match('/^Certificate Name: (.+)$/', $line, $matches)) {
+                if ($currentCert !== null) {
+                    $certificates[] = $currentCert;
+                }
+                $currentCert = ['name' => $matches[1]];
+
+            } elseif (preg_match('/^\s*Domains: (.+)$/', $line, $matches)) {
+                $currentCert['domains'] = explode(' ', $matches[1]);
+
+            } elseif (preg_match('/^\s*Expiry Date: (.+) \(VALID:/', $line, $matches)) {
+                $currentCert['expiry'] = $matches[1];
+            }
+        }
+
+        if ($currentCert !== null) {
+            $certificates[] = $currentCert;
+        }
+
+        return $certificates;
     }
 }
